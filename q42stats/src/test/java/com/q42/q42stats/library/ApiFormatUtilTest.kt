f02e1a36fcab5c7bc33b7fb0df8f6aaee58512c8
Nino van Hooff <nino@q42.nl>
2022-09-14 16:30:16 +0200

    BUGFIX ApiFormatUtilTest.kt
    bugfix/float-serialization-inconsistency

--- q42stats/src/test/java/com/q42/q42stats/library/ApiFormatUtilTest.kt
+++ q42stats/src/test/java/com/q42/q42stats/library/ApiFormatUtilTest.kt
@@ -1,15 +1,10 @@
 package com.q42.q42stats.library
 
-import com.q42.q42stats.library.util.deserializeMeasurement
-import com.q42.q42stats.library.util.serializeMeasurement
-import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ApiFormatUtilTest {
     @Test
     fun testToQ42StatsApiFormat() {
-        val expected =
-            """{"screen width":"360","fontSize":"1.0","textScale":"1.6","language":"nl","currentMeasurement":{"createdAt":"1337"},"talkbackEnabled":"true"}"""
         val actual = mapOf(
             "screen width" to 360,
             "fontSize" to 1.0, // expected is "1.0". should not be truncated to "1"
@@ -19,27 +14,21 @@ class ApiFormatUtilTest {
             "currentMeasurement" to mapOf(
                 "createdAt" to 1337L
             )
-        ).toQ42StatsApiFormat().toString()
-        assertEquals(expected, actual)
+        ).toQ42StatsApiFormat()
+
+        testQ42StatsMap(actual)
     }
 
-    @Test
-    fun testQ42StatsFormatCanBeSymmetricallySerialized() {
-        val expected =
-            """{"screen width":"360","fontSize":"1.0","textScale":"1.6","language":"nl","currentMeasurement":{"createdAt":"1337"},"talkbackEnabled":"true"}"""
-        val dataMap = mapOf(
-            "screen width" to 360,
-            "fontSize" to 1.0, // expected is "1.0". should not be truncated to "1"
-            "textScale" to 1.6,
-            "talkbackEnabled" to true,
-            "language" to "nl",
-            "currentMeasurement" to mapOf(
-                "createdAt" to 1337L
-            )
-        )
-        val statsFormat = dataMap.toQ42StatsApiFormat()
-        val jsonString = serializeMeasurement(statsFormat)
-        val actual = deserializeMeasurement(jsonString)
-        assertEquals(expected, actual)
+    private fun testQ42StatsMap(actual: Map<*, *>) {
+        actual.forEach {
+            when (it.value) {
+                is Map<*, *> -> {
+                    testQ42StatsMap(it.value as Map<*, *>)
+                }
+                else -> {
+                    assert(it.value is String) { "Expected $it to have a string type but was ${it.value}" }
+                }
+            }
+        }
     }
 }
