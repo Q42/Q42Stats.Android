f2184bc4549b3fa499f7dc8770f78553f7be0fc4
Nino van Hooff <nino@q42.nl>
2022-09-14 16:15:58 +0200

    ADD Q42Stats format documentation
    bugfix/float-serialization-inconsistency

--- q42stats/src/main/java/com/q42/q42stats/library/util/ApiFormatUtil.kt
+++ q42stats/src/main/java/com/q42/q42stats/library/util/ApiFormatUtil.kt
@@ -2,6 +2,11 @@ package com.q42.q42stats.library
 
 import java.io.Serializable
 
+/** Returns a version of the input where all values are stringified
+ *  We do this to prevent serialize -> deserialize issues where the float 1.0 is transformed to 1,
+ *  for example. (this change is undesirable because the changed data type causes the object to have
+ *  a different hash, not being equal in comparisons etc.
+ */
 internal fun Map<String, Any>.toQ42StatsApiFormat(): Map<String, Any> {
     val fireStoreMap = this.mapValues { entry ->
         mapFieldValue(entry)
